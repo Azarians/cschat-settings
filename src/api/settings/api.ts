ff0ef5d0b70e094cbd7fc33f198ff68b99959b8e
Azaryan-A <armana@boomte.ch>
2022-08-21 16:11:43 +0400

    save settings request method changed

--- src/api/settings/api.ts
+++ src/api/settings/api.ts
@@ -2,7 +2,7 @@ import { T_updateSettingsRequested } from '../../store/sagas/settings/types';
 
 const R_updateSettings = (body: T_updateSettingsRequested['payload']) => {
 	return fetch(`${process.env.REACT_APP_API_ROOT}settings`, {
-		method: 'POST',
+		method: 'PUT',
 		body: JSON.stringify(body),
 		headers: {
 			'content-type': 'application/json'
